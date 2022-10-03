808df05b5c7cdfc2e025cc13c5f83ec2b9710bb1
Jim Lougheed <jims.codes@gmail.com>
2022-10-03 15:30:51 -0400

    Add exit and level completion, loading next level

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+ [SerializeField] BoxCollider2D exitCollider;
+ int waitTime = 1;
+
+ void Start() {
+    exitCollider.GetComponent<BoxCollider2D>();
+ }
+
+ void OnTriggerEnter2D(Collider2D other) {
+    StartCoroutine(StartNextLevel());
+ }
+
+ IEnumerator StartNextLevel() {
+    yield return new WaitForSecondsRealtime(waitTime);
+    int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+    if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+        nextSceneIndex = 0;
+    }
+    SceneManager.LoadScene(nextSceneIndex);
+ }
+}
